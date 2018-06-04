69f3bd52faa8f9f3a8d43f9a2931fafd713a9aa1
santibarboza <santii.barboza@gmail.com>
2018-06-04 15:41:13 -0300

    Grupo twig

--- app/Http/Controllers/gruposController.php
+++ app/Http/Controllers/gruposController.php
@@ -6,9 +6,78 @@ use Illuminate\Http\Request;
 
 class gruposController extends Controller
 {
+	const columnas = array(
+		0 => array(
+			'size'=>"col-md-2",
+			'name'=>"Nombre",
+			'id'=>0
+		),
+		1 =>array(
+			'size'=>"col-md-1",
+			'name'=>"Codigo",
+			'id'=>1
+		),
+		2 =>array(
+			'size'=>"col-md-1",
+			'name'=>"Fecha de Creación",
+			'id'=>2
+		),
+		3 =>array(
+			'size'=>"col-md-1",
+			'name'=>"Horario de Inicio",
+			'id'=>3
+		),
+		4 =>array(
+			'size'=>"col-md-1",
+			'name'=>">Horario de Fin",
+			'id'=>4
+		),
+		5 =>array(
+			'size'=>"col-md-2",
+			'name'=>"Datos de Contacto",
+			'id'=>5
+		),
+		6 =>array(
+			'size'=>"col-md-1",
+			'name'=>"Religion",
+			'id'=>6
+		),
+		7 =>array(
+			'size'=>"col-md-1",
+			'name'=>"Ubicacion",
+			'id'=>7
+		),
+		8 =>array(
+			'size'=>"col-md-1",
+			'name'=>"Ramas",
+			'id'=>8
+		),
+		9 =>array(
+			'size'=>"col-md-1",
+			'name'=>"",
+			'id'=>9
+		),
+	);
+	const ubicacion='<a><span>Ver Ubicación</span></a>';
+	const ramas='<a><span>Ver Ramas</span></a>';
+	const edit='<a href="#editGrupoModal" class="edit" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Edit">&#xE254;</i></a><a href="#deleteGrupoModal" class="delete" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Delete">&#xE872;</i></a>';
+    
     public function index(){
-    	return view('index', [
-    		'title' => "Administrar Grupos"
+    	$grupos=[
+			0=>array("Coronel Rosales","234","25/10/1986","14:00","17:00","","Religión Heterogenea",
+						self::ubicacion,self::ramas,self::edit),
+			1=>array("San Antonio María Zaccaría","1035","24/09/2005","14:30","19:00"
+						,"<b>Sitio web: </b> https://www.facebook.com/samz1035/<br>
+                        <b>Telefono: </b>2121221","Católica",
+						self::ubicacion,self::ramas,self::edit),
+		];
+    	return view('grupos', [
+    		'title' => "Administrar Grupo Scouts",
+    		'titleTable'=> "Administrar Grupos Scouts",
+    		'addLabel'=>'Grupo',
+    		'modalDeleteTitle'=>" Eliminar Grupo",
+    		'columnas'=>self::columnas,
+    		'elementos'=>$grupos,
 		]);
     }
     
--- app/Http/Controllers/ramasController.php
+++ app/Http/Controllers/ramasController.php
@@ -6,20 +6,63 @@ use Illuminate\Http\Request;
 
 class ramasController extends Controller
 {
-    
-    
-    
+    const columnas = array(
+    		0 => array(
+    			'size'=>"col-md-2",
+    			'name'=>"Nombre",
+    			'id'=>0
+    		),
+    		1 =>array(
+    			'size'=>"col-md-1",
+    			'name'=>"Edad Mínima",
+    			'id'=>1
+    		),
+    		2 =>array(
+    			'size'=>"col-md-1",
+    			'name'=>"Edad Maxima",
+    			'id'=>2
+    		),
+    		3 =>array(
+    			'size'=>"col-md-2",
+    			'name'=>"Fecha de Inicio de Inscripcion",
+    			'id'=>3
+    		),
+    		4 =>array(
+    			'size'=>"col-md-2",
+    			'name'=>"Fecha de Fin de Inscripcion",
+    			'id'=>4
+    		),
+    		5 =>array(
+    			'size'=>"col-md-2",
+    			'name'=>"Tipo",
+    			'id'=>5
+    		),
+    		6 =>array(
+    			'size'=>"col-md-1",
+    			'name'=>"Fotos",
+    			'id'=>6
+    		),
+    		7 =>array(
+    			'size'=>"col-md-1",
+    			'name'=>"",
+    			'id'=>7
+    		),
+		);
+    const fotos="<a><span>Ver Fotos</span></a>";
+	const edit='<a href="#editRamaModal" class="edit" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Edit">&#xE254;</i></a><a href="#deleteRamaModal" class="delete" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Delete">&#xE872;</i></a>';
 
 	public function index($idgrupo){
-		$elementos=[
+		$ramas=[
+			0=>array("Manada Mowgli","6","11","1/03/2018","1/05/2018","Rama Mixta",self::fotos,self::edit),
+			1=>array("Unidad Traulen","11","13","1/03/2018","1/05/2018","Rama Mixta",self::fotos,self::edit),
 		];
     	return view('ramas', [
-    		'title' => "Administrar Ramas",
-    		'titleTable'=> 'Ramas del Grupo',
+    		'title' => "Administrar Ramas del Grupo ".$idgrupo,
+    		'titleTable'=> 'Ramas del Grupo '.$idgrupo,
     		'addLabel'=>'Rama',
     		'modalDeleteTitle'=>" Eliminar Rama",
-    		'columnas'=>[],
-    		'elementos'=>[]
+    		'columnas'=>self::columnas,
+    		'elementos'=>$ramas,
 		]);
     }
 }
