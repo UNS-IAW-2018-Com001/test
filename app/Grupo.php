c63c346b4d637f87e4533bcef87d72b9a5afe315
santibarboza <santii.barboza@gmail.com>
2018-06-06 14:39:41 -0300

    prueba

--- app/Grupo.php
+++ app/Grupo.php
@@ -2,8 +2,6 @@
 namespace App;
 use Jenssegers\Mongodb\Eloquent\Model as Eloquent;
 class Grupo extends Eloquent {
-
-    //protected $connection = 'mongodb';
     protected $collection = 'grupos';
 
 }
